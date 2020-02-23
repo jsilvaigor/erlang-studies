e92cd85697635423a24d5783a34e4021c321d8d7
Igor Silva <igorjose15@gmail.com>
2020-02-23 13:22:50 -0300

    Records

---
+++ chapter5/records.hrl
@@ -0,0 +1,11 @@
+%%%-------------------------------------------------------------------
+%%% @author igorsilva
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23. Feb 2020 12:11 PM
+%%%-------------------------------------------------------------------
+-author("igorsilva").
+
+-record(todo, {status = reminder, who = joe, text}).
